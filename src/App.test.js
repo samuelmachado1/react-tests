752d78cb58f5f36ad082b10a5fc0dfc881d9d6d8
smachado <samuelmachado1@gmail.com>
2021-02-12 00:25:51 -0300

    feat(tests): Organizing the test file better

--- src/App.test.js
+++ src/App.test.js
@@ -4,9 +4,22 @@ import { render, screen } from '@testing-library/react';
 import App from './app';
 
 describe('Componente principal', () => {
-    it('Mostrar o nome do banco', () => {
-        render(<App />);
-
-        expect(screen.getByText('ByteBank')).toBeInTheDocument();
+    describe('Quando abro o app do Banco', () => {
+        it('O nome é exibido', () => {
+            render(<App />);
+    
+            expect(screen.getByText('ByteBank')).toBeInTheDocument();
+        })
+        it('O saldo é exibido', () => {
+            render(<App />);
+    
+            expect(screen.getByText('Saldo:')).toBeInTheDocument();
+        })
+        it('O botão de realizar transação é exibido', () => {
+            render(<App />);
+    
+            expect(screen.getByText('Realizar operação')).toBeInTheDocument();
+        })  
     })
+    
 })
