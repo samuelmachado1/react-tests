0228b9c19901076a7d3a6da46213b3767763df73
Samuel Machado <samuelmachado1@gmail.com>
2021-09-22 20:56:34 -0300

    feat(test): utilizing fireEvent

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import App, { calcularNovoSaldo } from "./app";
 // Teste de Componente -  Testando o componente <App/>
@@ -43,6 +43,22 @@ describe("Componente principal", () => {
       const novoSaldo = calcularNovoSaldo(valores, 90);
 
       expect(novoSaldo).toBe(100);
+    });
+    it("que é um saque, a transação deve ser realizada", () => {
+      const { getByText, getByLabelText, getByTestId } = render(<App />);
+
+      const saldo = getByText("R$ 1000");
+      const transacao = getByLabelText("Saque");
+      const valor = getByTestId("valor");
+      const botaoTransacao = getByText("Realizar operação");
+
+      expect(saldo.textContent).toBe("R$ 1000");
+
+      fireEvent.click(transacao, { target: { value: "saque" } });
+      fireEvent.change(valor, { target: { value: "10.00" } });
+      fireEvent.click(botaoTransacao);
+
+      expect(saldo.textContent).toBe("R$ 990");
     });
   });
 });
