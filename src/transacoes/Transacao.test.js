cf9026cc0477ae4761245b84434199a8aa0b6f69
Samuel Machado <samuelmachado1@gmail.com>
2021-09-22 19:56:05 -0300

    feat(test): adjusting snapshot tests

--- src/transacoes/Transacao.test.js
+++ src/transacoes/Transacao.test.js
@@ -5,7 +5,7 @@ import Transacao from "./Transacao";
 describe("Componente de transação do extrato", () => {
   it("O Snapshot do component deve permanecer sempre o mesmo", () => {
     const { container } = render(
-      <Transacao data="08/09/2020" tipo="saque" valor="R$ 20.00" />
+      <Transacao data="08/09/2020" tipo="saque" valor="20.00" />
     );
 
     expect(container.firstChild).toMatchSnapshot();
