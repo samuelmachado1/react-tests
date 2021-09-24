8a57ef1783f3f4eca1cb1798f39e4c6bf507d9a4
Samuel Machado <samuelmachado@ciandt.com>
2021-09-24 14:06:52 -0300

    feat(tests) adjusting snapshot test

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -4,7 +4,7 @@ import Conta from "./Conta";
 
 describe("Componente de saldo da conta", () => {
   it("O Snapshot do component deve permanecer sempre o mesmo", () => {
-    const { container } = render(<Conta saldo="100" />);
+    const { container } = render(<Conta saldo={100} />);
 
     expect(container.firstChild).toMatchSnapshot();
   });
