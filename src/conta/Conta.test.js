bb91726adce180be839d479763eabae02fc8f568
Samuel Machado <samuelmachado@ciandt.com>
2021-09-24 17:15:43 -0300

    feat(tests) Update Conta.test. Implementing field value test

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Conta from "./Conta";
 
 describe("Componente de conta", () => {
@@ -9,6 +9,19 @@ describe("Componente de conta", () => {
     const saldo = screen.getByTestId("saldo-conta");
 
     expect(saldo.textContent).toBe("R$ 1000");
+  });
+
+  it("Chama a função de realizar transação, quando o botão é clicado", () => {
+    const funcaoRealizarTransacao = jest.fn();
+
+    render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao} />);
+
+    const valor = screen.getByTestId("valor");
+
+    fireEvent.change(valor, { target: { value: 1 } });
+    fireEvent.click(screen.getByText("Realizar operação"));
+
+    expect(funcaoRealizarTransacao).toHaveBeenCalled();
   });
 });
 
