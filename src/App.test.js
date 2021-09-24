cc8d26796d50ed2a69399bfa90f67127097a65e3
Samuel Machado <samuelmachado@ciandt.com>
2021-09-24 14:46:15 -0300

    feat(tests) adjusting render methods

--- src/App.test.js
+++ src/App.test.js
@@ -15,6 +15,13 @@ describe("Componente principal", () => {
 
       expect(screen.getByText("Saldo:")).toBeInTheDocument();
     });
+    it("O campo valor é exibido com nº 0 por default", () => {
+      render(<App />);
+      const valor = screen.getByTestId("valor");
+
+      expect(screen.getByText("Valor:")).toBeInTheDocument();
+      expect(valor.textContent).toBe("");
+    });
     it("O botão de realizar transação é exibido", () => {
       render(<App />);
 
@@ -34,6 +41,7 @@ describe("Componente principal", () => {
 
       expect(novoSaldo).toBe(100);
     });
+
     it("que é um depósito, o valor vai aumentar", () => {
       const valores = {
         transacao: "deposito",
@@ -45,12 +53,12 @@ describe("Componente principal", () => {
       expect(novoSaldo).toBe(100);
     });
     it("que é um saque, a transação deve ser realizada", () => {
-      const { getByText, getByLabelText, getByTestId } = render(<App />);
+      render(<App />);
 
-      const saldo = getByText("R$ 1000");
-      const transacao = getByLabelText("Saque");
-      const valor = getByTestId("valor");
-      const botaoTransacao = getByText("Realizar operação");
+      const saldo = screen.getByText("R$ 1000");
+      const transacao = screen.getByLabelText("Saque");
+      const valor = screen.getByTestId("valor");
+      const botaoTransacao = screen.getByText("Realizar operação");
 
       expect(saldo.textContent).toBe("R$ 1000");
 
