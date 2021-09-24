1aae92bbf047b1173cdc0ed4ab07981e710687ea
Samuel Machado <samuelmachado@ciandt.com>
2021-09-24 15:25:07 -0300

    feat(tests) adjusting Conta component test

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Conta from "./Conta";
 
+describe("Componente de conta", () => {
+  it("Exibir o saldo da conta como valor monetário", () => {
+    render(<Conta saldo={1000} />);
+
+    const saldo = screen.getByTestId("saldo-conta");
+
+    expect(saldo.textContent).toBe("R$ 1000");
+  });
+});
+
 describe("Componente de saldo da conta", () => {
-  it("O Snapshot do component deve permanecer sempre o mesmo", () => {
+  it("O Snapshot do componente deve permanecer sempre o mesmo", () => {
     const { container } = render(<Conta saldo={100} />);
 
     expect(container.firstChild).toMatchSnapshot();
